854e7b165205314bbf72e3e3078343fae68d9925
madarasz <istvan.madarasz@disney.com>
2016-05-10 15:23:38 +0200

    create tournament form updates

---
+++ app/Http/Controllers/TournamentsController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tournament;
+use Illuminate\Http\Request;
+use DB;
+
+use App\Http\Requests;
+
+class TournamentsController extends Controller
+{
+    public function create(Request $request) {
+        $this->validate($request, [
+            'title' => 'required',
+            'date' => 'required|date_format:Y.m.d.',
+            'location_city' => 'required',
+            'players_number' => 'integer|between:1,1000',
+            'top_number' => 'integer|between:0,1000',
+            'location_country' => 'not_in:0',
+        ]);
+
+        DB::table('tournaments')->insert([
+            'tournament_type_id' => $request->tournament_type_id,
+            'location_country' => $request->location_country,
+            'location_us_state' => $request->location_us_state,
+            'title' => $request->title,
+            'date' => $request->date,
+            'time' => $request->time,
+            'location_city' => $request->location_city,
+            'location_store' => $request->location_store,
+            'location_address' => $request->location_address,
+            'concluded' => $request->concluded === '' ? 1 : 0,
+            'players_number' => $request->concluded === '' ? $request->has('players_number') : 0,
+            'top_number' => $request->concluded === '' ? $request->has('top_number') : 0,
+            'description' => $request->description,
+            'decklist' => $request->decklist === '' ? 1 : 0,
+            'creator' => 0 // TODO: creator
+
+        ]);
+        return $request->all(); // TODO: page redirect
+    }
+
+    public function edit(Tournament $tournament) {
+        return view('meh', compact('tournament'));
+    }
+
+    public function update(Request $request, Tournament $tournament) {
+        $tournament->update($request->all());
+    }
+}
