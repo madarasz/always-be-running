66fe58936b388f1557784fce6f9197571d9325e9
madarasz <istvan.madarasz@disney.com>
2016-05-19 11:41:34 +0200

    XSS protection

---
+++ app/Http/Middleware/XSSProtection.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class XSSProtection
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!in_array(strtolower($request->method()), ['put', 'post', 'patch'])) {
+            return $next($request);
+        }
+
+
+        $input = $request->all();
+
+        array_walk_recursive($input, function(&$input) {
+            $input = strip_tags($input);
+        });
+
+        $request->merge($input);
+
+        return $next($request);
+    }
+}
