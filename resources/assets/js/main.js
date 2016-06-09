c8515cc483792b1c3dd4dae01ceba50d4b3b1fc9
madarasz <istvan.madarasz@disney.com>
2016-06-09 12:40:03 +0200

    calendar functionality

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -48,3 +48,302 @@ var delay = (function(){
         timer = setTimeout(callback, ms);
     };
 })();
+
+function getTournamentData(filters, callback) {
+    $.ajax({
+        url: '/api/tournaments?' + filters,
+        dataType: "json",
+        async: true,
+        success: function (data) {
+            callback(data);
+        }
+    });
+}
+
+function updateTournamentTable(elementID, columns, emptyMessage, data) {
+    $(elementID).find('tbody').empty();
+    $.each(data, function (index, element) {
+        newrow = $('<tr>').appendTo(elementID + ' > tbody');
+
+        // if zero rows
+        if (data.length == 0) {
+            newrow.append($('<td>', {
+                text: emptyMessage,
+                colspan: columns.length,
+                'class': 'text-center'
+            }));
+            return 0;
+        }
+
+        // title
+        if ($.inArray('title', columns) > -1) {
+            newrow.append($('<td>').append($('<a>', {
+                text: element.title,
+                href: '/tournaments/' + element.id
+            })));
+        }
+        // date
+        if ($.inArray('date', columns) > -1) {
+            newrow.append($('<td>', {
+                text: element.date
+            }));
+        }
+        // location
+        if ($.inArray('location', columns) > -1) {
+            newrow.append($('<td>', {
+                text: element.location
+            }));
+        }
+        // cardpool
+        if ($.inArray('cardpool', columns) > -1) {
+            newrow.append($('<td>', {
+                text: element.cardpool
+            }));
+        }
+        // approved
+        if ($.inArray('approval', columns) > -1) {
+            cell = $('<td>', {
+                'class': 'text-center'
+            }).appendTo(newrow);
+
+            if (element.approved === null) {
+                cell.append($('<span>', {
+                    text: 'pending',
+                    'class': 'label label-warning'
+                }));
+            } else if (element.approved) {
+                cell.append($('<span>', {
+                    text: 'approved',
+                    'class': 'label label-success'
+                }));
+            } else {
+                cell.append($('<i>', {
+                    'aria-hidden': true,
+                    'class': 'fa fa-thumbs-down text-danger'
+                }), ' ', $('<span>', {
+                    text: 'rejected',
+                    'class': 'label label-danger'
+                }));
+            }
+        }
+        // claim
+        if ($.inArray('user_claim', columns) > -1) {
+            cell = $('<td>', {
+                'class': 'text-center'
+            }).appendTo(newrow);
+
+            if (element.user_claim) {
+                cell.append($('<span>', {
+                    text: 'claimed',
+                    'class': 'label label-success'
+                }));
+            } else if (element.concluded) {
+                cell.append($('<i>', {
+                    'aria-hidden': true,
+                    'class': 'fa fa-clock-o text-danger'
+                }), ' ', $('<span>', {
+                    text: 'please claim',
+                    'class': 'label label-danger'
+                }));
+            } else {
+                cell.append($('<span>', {
+                    text: 'registered',
+                    'class': 'label label-info'
+                }));
+            }
+        }
+        // conclusion
+        if ($.inArray('conclusion', columns) > -1) {
+            cell = $('<td>', {
+                'class': 'text-center'
+            }).appendTo(newrow);
+
+            if (element.concluded) {
+                cell.append($('<span>', {
+                    text: 'concluded',
+                    'class': 'label label-success'
+                }));
+            } else if (element.date <= '{{ $nowdate }}') {
+                cell.append($('<i>', {
+                    'aria-hidden': true,
+                    'class': 'fa fa-clock-o text-danger'
+                }), ' ', $('<span>', {
+                    text: 'due, pls update',
+                    'class': 'label label-danger'
+                }));
+            } else {
+                cell.append($('<span>', {
+                    text: 'not yet',
+                    'class': 'label label-info'
+                }));
+            }
+        }
+        // players
+        if ($.inArray('players', columns) > -1) {
+            newrow.append($('<td>', {
+                text: element.concluded ? element.players_count : element.registration_count,
+                'class': 'text-center'
+            }));
+        }
+        // claims
+        if ($.inArray('claims', columns) > -1) {
+            cell = $('<td>', {
+                'class': 'text-center'
+            }).appendTo(newrow);
+
+            if (element.claim_conflict) {
+                cell.append($('<i>', {
+                    'title': 'conflict',
+                    'class': 'fa fa-exclamation-triangle text-danger'
+                }), ' ');
+            }
+
+            cell.append(element.claim_count);
+
+        }
+        // action_edit
+        if ($.inArray('action_edit', columns) > -1) {
+            newrow.append($('<td>').append($('<a>', {
+                'class': 'btn btn-primary btn-xs',
+                href: '/tournaments/' + element.id + '/edit'
+            }).append($('<i>', {
+                'class': 'fa fa-pencil',
+                'aria-hidden': true
+            }), ' edit')));
+        }
+        // action_approve
+        if ($.inArray('action_approve', columns) > -1) {
+            newrow.append($('<td>').append($('<a>', {
+                'class': 'btn btn-success btn-xs',
+                href: '/tournaments/' + element.id + '/approve'
+            }).append($('<i>', {
+                'class': 'fa fa-thumbs-up',
+                'aria-hidden': true
+            }), ' approve')));
+        }
+        // action_reject
+        if ($.inArray('action_reject', columns) > -1) {
+            newrow.append($('<td>').append($('<a>', {
+                'class': 'btn btn-danger btn-xs',
+                href: '/tournaments/' + element.id + '/reject'
+            }).append($('<i>', {
+                'class': 'fa fa-thumbs-down',
+                'aria-hidden': true
+            }), ' reject')));
+        }
+        // action_restore
+        if ($.inArray('action_restore', columns) > -1) {
+            newrow.append($('<td>').append($('<a>', {
+                'class': 'btn btn-primary btn-xs',
+                href: '/tournaments/' + element.id + '/restore'
+            }).append($('<i>', {
+                'class': 'fa fa-recycle',
+                'aria-hidden': true
+            }), ' restore')));
+        }
+        // action_delete
+        if ($.inArray('action_delete', columns) > -1) {
+            newrow.append($('<td>').append($('<form>', {
+                method: 'POST',
+                action: '/tournaments/' + element.id
+            }).append($('<input>', {
+                name: '_method',
+                type: 'hidden',
+                value: 'DELETE'
+            }), $('<input>', {
+                name: '_token',
+                type: 'hidden',
+                value: '{{ csrf_token() }}'
+            }), $('<button>', {
+                type: 'submit',
+                'class': 'btn btn-danger btn-xs'
+            }).append($('<i>', {
+                'class': 'fa fa-trash',
+                'aria-hidden': true
+            }), ' delete'))));
+        }
+
+    }, columns, emptyMessage);
+}
+
+function convertDateForCalendar(dataString) {
+    return dataString.substr(5,2) + '-' + dataString.substr(8,2) + '-' + dataString.substr(0,4);
+}
+
+function leadingZero(number) {
+    if (number < 10) {
+        return '0' + number;
+    } else {
+        return number;
+    }
+}
+
+function updateTournamentCalendar(data) {
+    var calendardata = {};
+    $.each(data, function (index, element) {
+        var entry = '<a href="/tournaments/' + element.id + '">' + element.title + '</a><small>' + element.location + '</small>';
+        if (typeof calendardata[convertDateForCalendar(element.date)] == "undefined") {
+            calendardata[convertDateForCalendar(element.date)] = [entry];
+        } else {
+            calendardata[convertDateForCalendar(element.date)].push(entry);
+        }
+    });
+
+    $(function() {
+
+        var $wrapper = $( '#custom-inner' ),
+            $calendar = $( '#calendar' ),
+            cal = $calendar.calendario( {
+                onDayClick : function( $el, $contentEl, dateProperties ) {
+
+                    if( $contentEl.content.length > 0 ) {
+                        showEvents( $contentEl.content, dateProperties );
+                    }
+
+                },
+                caldata : calendardata,
+                displayWeekAbbr : true
+            } ),
+            $month = $( '#custom-month' ).html( cal.getMonthName() ),
+            $year = $( '#custom-year' ).html( cal.getYear() );
+
+        $( '#custom-next' ).on( 'click', function() {
+            cal.gotoNextMonth( updateMonthYear );
+        } );
+        $( '#custom-prev' ).on( 'click', function() {
+            cal.gotoPreviousMonth( updateMonthYear );
+        } );
+
+        function updateMonthYear() {
+            $month.html( cal.getMonthName() );
+            $year.html( cal.getYear() );
+        }
+
+        function showEvents( $contentEl, dateProperties ) {
+
+            hideEvents();
+
+            var $events = $( '<div id="custom-content-reveal" class="custom-content-reveal"><h4>Tournaments for ' + dateProperties.year + '.' + leadingZero(dateProperties.month) + '.' + leadingZero(dateProperties.day) + '.</h4></div>' ),
+                $close = $( '<span class="custom-content-close"></span>' ).on( 'click', hideEvents );
+
+            $events.append( $contentEl , $close ).insertAfter( $wrapper );
+
+            setTimeout( function() {
+                $events.css( 'top', '0%' );
+            }, 10 );
+
+        }
+        function hideEvents() {
+
+            var $events = $( '#custom-content-reveal' );
+            if( $events.length > 0 ) {
+
+                $events.css( 'top', '100%' );
+                $events.remove();
+
+            }
+
+        }
+
+    });
+}
--- resources/views/discover.blade.php
+++ resources/views/discover.blade.php
@@ -41,229 +41,16 @@
                 ['columns' => ['title', 'location', 'date', 'players', 'cardpool'],
                 'data' => $tournaments, 'title' => 'Upcoming tournaments',
                  'id' => 'discover-table', 'icon' => 'fa-list-alt'])
-                <script type="text/javascript">
-                    $.ajax({
-                        url: '/api/tournaments?user=1276',
-                        dataType: "json",
-                        async: true,
-                        success: function (data) {
-                            updateTournamentTable(data, ['title', 'date', 'location', 'cardpool', 'players'], 'no tournaments to show');
-                        }
-                    });
-
-                    function updateTournamentTable(data, columns, emptyMessage) {
-                        $('#discover-table').find('tbody').empty();
-                        $.each(data, function (index, element) {
-                            newrow = $('<tr>').appendTo('#discover-table > tbody');
-
-                            // if zero rows
-                            if (data.length == 0) {
-                                newrow.append($('<td>', {
-                                    text: emptyMessage,
-                                    colspan: columns.length,
-                                    'class': 'text-center'
-                                }));
-                                return 0;
-                            }
-
-                            // title
-                            if ($.inArray('title', columns) > -1) {
-                                newrow.append($('<td>').append($('<a>', {
-                                    text: element.title,
-                                    href: '/tournaments/' + element.id
-                                })));
-                            }
-                            // date
-                            if ($.inArray('date', columns) > -1) {
-                                newrow.append($('<td>', {
-                                    text: element.date
-                                }));
-                            }
-                            // location
-                            if ($.inArray('location', columns) > -1) {
-                                newrow.append($('<td>', {
-                                    text: element.location
-                                }));
-                            }
-                            // cardpool
-                            if ($.inArray('cardpool', columns) > -1) {
-                                newrow.append($('<td>', {
-                                    text: element.cardpool
-                                }));
-                            }
-                            // approved
-                            if ($.inArray('approval', columns) > -1) {
-                                cell = $('<td>', {
-                                    'class': 'text-center'
-                                }).appendTo(newrow);
-
-                                if (element.approved === null) {
-                                    cell.append($('<span>', {
-                                        text: 'pending',
-                                        'class': 'label label-warning'
-                                    }));
-                                } else if (element.approved) {
-                                    cell.append($('<span>', {
-                                        text: 'approved',
-                                        'class': 'label label-success'
-                                    }));
-                                } else {
-                                    cell.append($('<i>', {
-                                        'aria-hidden': true,
-                                        'class': 'fa fa-thumbs-down text-danger'
-                                    }), ' ', $('<span>', {
-                                        text: 'rejected',
-                                        'class': 'label label-danger'
-                                    }));
-                                }
-                            }
-                            // claim
-                            if ($.inArray('user_claim', columns) > -1) {
-                                cell = $('<td>', {
-                                    'class': 'text-center'
-                                }).appendTo(newrow);
-
-                                if (element.user_claim) {
-                                    cell.append($('<span>', {
-                                        text: 'claimed',
-                                        'class': 'label label-success'
-                                    }));
-                                } else if (element.concluded) {
-                                    cell.append($('<i>', {
-                                        'aria-hidden': true,
-                                        'class': 'fa fa-clock-o text-danger'
-                                    }), ' ', $('<span>', {
-                                        text: 'please claim',
-                                        'class': 'label label-danger'
-                                    }));
-                                } else {
-                                    cell.append($('<span>', {
-                                        text: 'registered',
-                                        'class': 'label label-info'
-                                    }));
-                                }
-                            }
-                            // conclusion
-                            if ($.inArray('conclusion', columns) > -1) {
-                                cell = $('<td>', {
-                                    'class': 'text-center'
-                                }).appendTo(newrow);
-
-                                if (element.concluded) {
-                                    cell.append($('<span>', {
-                                        text: 'concluded',
-                                        'class': 'label label-success'
-                                    }));
-                                } else if (element.date <= '{{ $nowdate }}') {
-                                    cell.append($('<i>', {
-                                        'aria-hidden': true,
-                                        'class': 'fa fa-clock-o text-danger'
-                                    }), ' ', $('<span>', {
-                                        text: 'due, pls update',
-                                        'class': 'label label-danger'
-                                    }));
-                                } else {
-                                    cell.append($('<span>', {
-                                        text: 'not yet',
-                                        'class': 'label label-info'
-                                    }));
-                                }
-                            }
-                            // players
-                            if ($.inArray('players', columns) > -1) {
-                                newrow.append($('<td>', {
-                                    text: element.concluded ? element.players_count : element.registration_count,
-                                    'class': 'text-center'
-                                }));
-                            }
-                            // claims
-                            if ($.inArray('claims', columns) > -1) {
-                                cell = $('<td>', {
-                                    'class': 'text-center'
-                                }).appendTo(newrow);
-
-                                if (element.claim_conflict) {
-                                    cell.append($('<i>', {
-                                        'title': 'conflict',
-                                        'class': 'fa fa-exclamation-triangle text-danger'
-                                    }), ' ');
-                                }
-
-                                cell.append(element.claim_count);
-
-                            }
-                            // action_edit
-                            if ($.inArray('action_edit', columns) > -1) {
-                                newrow.append($('<td>').append($('<a>', {
-                                    'class': 'btn btn-primary btn-xs',
-                                    href: '/tournaments/' + element.id + '/edit'
-                                }).append($('<i>', {
-                                    'class': 'fa fa-pencil',
-                                    'aria-hidden': true
-                                }), ' edit')));
-                            }
-                            // action_approve
-                            if ($.inArray('action_approve', columns) > -1) {
-                                newrow.append($('<td>').append($('<a>', {
-                                    'class': 'btn btn-success btn-xs',
-                                    href: '/tournaments/' + element.id + '/approve'
-                                }).append($('<i>', {
-                                    'class': 'fa fa-thumbs-up',
-                                    'aria-hidden': true
-                                }), ' approve')));
-                            }
-                            // action_reject
-                            if ($.inArray('action_reject', columns) > -1) {
-                                newrow.append($('<td>').append($('<a>', {
-                                    'class': 'btn btn-danger btn-xs',
-                                    href: '/tournaments/' + element.id + '/reject'
-                                }).append($('<i>', {
-                                    'class': 'fa fa-thumbs-down',
-                                    'aria-hidden': true
-                                }), ' reject')));
-                            }
-                            // action_restore
-                            if ($.inArray('action_restore', columns) > -1) {
-                                newrow.append($('<td>').append($('<a>', {
-                                    'class': 'btn btn-primary btn-xs',
-                                    href: '/tournaments/' + element.id + '/restore'
-                                }).append($('<i>', {
-                                    'class': 'fa fa-recycle',
-                                    'aria-hidden': true
-                                }), ' restore')));
-                            }
-                            // action_delete
-                            if ($.inArray('action_delete', columns) > -1) {
-                                newrow.append($('<td>').append($('<form>', {
-                                    method: 'POST',
-                                    action: '/tournaments/' + element.id
-                                }).append($('<input>', {
-                                    name: '_method',
-                                    type: 'hidden',
-                                    value: 'DELETE'
-                                }), $('<input>', {
-                                    name: '_token',
-                                    type: 'hidden',
-                                    value: '{{ csrf_token() }}'
-                                }), $('<button>', {
-                                    type: 'submit',
-                                    'class': 'btn btn-danger btn-xs'
-                                }).append($('<i>', {
-                                    'class': 'fa fa-trash',
-                                    'aria-hidden': true
-                                }), ' delete'))));
-                            }
-
-                        }, columns, emptyMessage);
-                    }
-                </script>
             </div>
         </div>
     </div>
     <div class="row">
         <div class="col-md-6 col-xs-12">
             <div class="bracket">
-                <h4><i class="fa fa-calendar" aria-hidden="true"></i> Upcoming calendar</h4>
+                <h4>
+                    <i class="fa fa-calendar" aria-hidden="true"></i> Upcoming calendar<br/>
+                    <small>past events are hidden</small>
+                </h4>
                 <div class="custom-calendar-wrap">
                     <div id="custom-inner" class="custom-inner">
                         <div class="custom-header clearfix">
@@ -279,63 +66,6 @@
                 </div>
                 <script type="application/javascript">
 
-                    $(function() {
-
-                        var $wrapper = $( '#custom-inner' ),
-                                $calendar = $( '#calendar' ),
-                                cal = $calendar.calendario( {
-                                    onDayClick : function( $el, $contentEl, dateProperties ) {
-
-                                        if( $contentEl.content.length > 0 ) {
-                                            showEvents( $contentEl.content, dateProperties );
-                                        }
-
-                                    },
-                                    caldata : { '06-09-2016': ['<a href="">event one</a>', '<span>event two</span>']},
-                                    displayWeekAbbr : true
-                                } ),
-                                $month = $( '#custom-month' ).html( cal.getMonthName() ),
-                                $year = $( '#custom-year' ).html( cal.getYear() );
-
-                        $( '#custom-next' ).on( 'click', function() {
-                            cal.gotoNextMonth( updateMonthYear );
-                        } );
-                        $( '#custom-prev' ).on( 'click', function() {
-                            cal.gotoPreviousMonth( updateMonthYear );
-                        } );
-
-                        function updateMonthYear() {
-                            $month.html( cal.getMonthName() );
-                            $year.html( cal.getYear() );
-                        }
-
-                        function showEvents( $contentEl, dateProperties ) {
-
-                            hideEvents();
-
-                            var $events = $( '<div id="custom-content-reveal" class="custom-content-reveal"><h4>Tournaments for ' + dateProperties.year + ' ' + dateProperties.monthname + ' ' + dateProperties.day + '</h4></div>' ),
-                                    $close = $( '<span class="custom-content-close"></span>' ).on( 'click', hideEvents );
-
-                            $events.append( $contentEl , $close ).insertAfter( $wrapper );
-
-                            setTimeout( function() {
-                                $events.css( 'top', '0%' );
-                            }, 10 );
-
-                        }
-                        function hideEvents() {
-
-                            var $events = $( '#custom-content-reveal' );
-                            if( $events.length > 0 ) {
-
-                                $events.css( 'top', '100%' );
-                                $events.remove();
-
-                            }
-
-                        }
-
-                    });
 
 
                 </script>
@@ -349,5 +79,13 @@
             </div>
         </div>
     </div>
+    <script type="text/javascript">
+
+        getTournamentData('', function(data) {
+            updateTournamentTable('#discover-table', ['title', 'date', 'location', 'cardpool', 'players'], 'no tournaments to show', data);
+            updateTournamentCalendar(data);
+        });
+
+    </script>
 @stop
 
