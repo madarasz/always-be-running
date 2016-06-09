9dcbbc838a5d7bad6d9375b7b194d9bea8dfb839
madarasz <istvan.madarasz@disney.com>
2016-06-09 14:47:36 +0200

    calendar css fixes

--- resources/views/discover.blade.php
+++ resources/views/discover.blade.php
@@ -51,24 +51,12 @@
                     <i class="fa fa-calendar" aria-hidden="true"></i> Upcoming calendar<br/>
                     <small>past events are hidden</small>
                 </h4>
-                <div class="custom-calendar-wrap">
-                    <div id="custom-inner" class="custom-inner">
-                        <div class="custom-header clearfix">
-                            <nav>
-                                <span id="custom-prev" class="custom-prev"></span>
-                                <span id="custom-next" class="custom-next"></span>
-                            </nav>
-                            <h2 id="custom-month" class="custom-month"></h2>
-                            <h3 id="custom-year" class="custom-year"></h3>
-                        </div>
-                        <div id="calendar" class="fc-calendar-container"></div>
-                    </div>
-                </div>
-                <script type="application/javascript">
-
-
-
-                </script>
+                <style type="text/css">
+                    .fc-past {
+                        cursor: default !important; background: transparent !important;
+                    }
+                </style>
+                @include('partials.calendar')
             </div>
         </div>
         <div class="col-md-6 col-xs-12">
