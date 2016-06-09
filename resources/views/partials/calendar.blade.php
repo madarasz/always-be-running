e1688f4a6075a7078ca1b465e48f7e229d2ef819
madarasz <istvan.madarasz@disney.com>
2016-06-09 15:23:28 +0200

    calendar fix

---
+++ resources/views/partials/calendar.blade.php
@@ -0,0 +1,13 @@
+<div class="custom-calendar-wrap">
+    <div id="custom-inner" class="custom-inner">
+        <div class="custom-header clearfix">
+            <nav>
+                <span id="custom-prev" class="custom-prev"></span>
+                <span id="custom-next" class="custom-next"></span>
+            </nav>
+            <h2 id="custom-month" class="custom-month"></h2>
+            <h3 id="custom-year" class="custom-year"></h3>
+        </div>
+        <div id="calendar" class="fc-calendar-container"></div>
+    </div>
+</div>
