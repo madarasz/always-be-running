3c83bb354bcbc069bd932ab4ef7f6b578810e014
madarasz <istvan.madarasz@disney.com>
2016-09-20 17:57:23 +0200

    install instructions

--- readme.md
+++ readme.md
@@ -1,3 +1,41 @@
-# Always be running . net
+# AlwaysBeRunning.net (ABR)
 
-Sorry, no docs yet.
+### Installation (Linux/Mac)
+
+You will need the following in order to run ABR locally:
+- MySQL (preferably)
+- PHP
+- PHP Composer
+- Node, NPM
+- JQ - download via apt-get (Debian) or homebrew (Mac), this is NOT an npm module
+- imagemagick
+
+1. Clone ABR from GitHub
+2. Install npm dependencies, install npm gulp globally
+
+    npm install
+    npm install -g gulp
+
+3. Install PHP dependencies
+
+    php composer.phar install
+
+4. Configure the settings of your local environment. Rename the **.example.env** file to **.env**. Edit the DB settings to connect to your locally running DB. *Ask the main dev (madarasz / Necro) for NetrunnerDB and Google API keys*. Everything else should be fine.
+5. Prepare ID icons by running this script (downloads from NetrunnerDB, run it regularly)
+
+    ./get_id_images.sh
+
+6. Prepare DB tables
+
+    php artisan migrate
+    php artisan db:seed
+
+7. Run the webapp. It should be available at [http://localhost:8000](http://localhost:8000) afterwards.
+
+    php artisan serve
+
+8. Make yourself an admin. Go to the webapp in your browser. Login via NetrunnerDB to enter your user in the DB. Check your DB (use phpMyAdmin), in table **users** set the **admin** value of your user to 1. If you reload the webapp you should see the **Admin** section in the top menu.
+
+9. Download all the data required from NetrunnerDB. Go to [Admin section](http://localhost:8000/admin) and click the **Update Card cycles**, **Update Card packs** and **Update Identities** buttons to get the data.
+
+10. You are done :)
---
+++ vendor/lusitanian/oauth/src/OAuth/OAuth2/Service/NetrunnerDB.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace OAuth\OAuth2\Service;
+
+use Illuminate\Support\Facades\Auth;
+use OAuth\Common\Token\TokenInterface;
+use OAuth\OAuth2\Token\StdOAuth2Token;
+use OAuth\Common\Http\Exception\TokenResponseException;
+use OAuth\Common\Http\Uri\Uri;
+use OAuth\Common\Consumer\CredentialsInterface;
+use OAuth\Common\Http\Client\ClientInterface;
+use OAuth\Common\Storage\TokenStorageInterface;
+use OAuth\Common\Http\Uri\UriInterface;
+use OAuth\Common\Token\Exception\ExpiredTokenException;
+
+class NetrunnerDB extends AbstractService
+{
+
+    public function __construct(
+        CredentialsInterface $credentials,
+        ClientInterface $httpClient,
+        TokenStorageInterface $storage,
+        $scopes = array(),
+        UriInterface $baseApiUri = null
+    ) {
+        parent::__construct($credentials, $httpClient, $storage, $scopes, $baseApiUri);
+
+        if (null === $baseApiUri) {
+            $this->baseApiUri = new Uri('https://netrunnerdb.com/api/2.0/');
+        }
+    }
+
+    /**
+     * Parses the access token response and returns a TokenInterface.
+     *
+     *
+     * @param string $responseBody
+     *
+     * @return TokenInterface
+     *
+     * @throws TokenResponseException
+     */
+    protected function parseAccessTokenResponse($responseBody)
+    {
+        $data = json_decode($responseBody, true);
+
+        if (null === $data || !is_array($data)) {
+            throw new TokenResponseException('Unable to parse response.');
+        } elseif (isset($data['error'])) {
+            throw new TokenResponseException('Error in retrieving token: "' . $data['error'] . '"');
+        }
+        $token = new StdOAuth2Token();
+        $token->setAccessToken($data['access_token']);
+        if (isset($data['expires_in'])) {
+            $token->setLifeTime($data['expires_in']);
+            unset($data['expires_in']);
+        }
+
+        if (isset($data['refresh_token'])) {
+            $token->setRefreshToken($data['refresh_token']);
+            unset($data['refresh_token']);
+        }
+        unset($data['access_token']);
+
+        $token->setExtraParams($data);
+        return $token;
+    }
+
+    /**
+     * Returns the authorization API endpoint.
+     *
+     * @return UriInterface
+     */
+    public function getAuthorizationEndpoint()
+    {
+        return new Uri('https://netrunnerdb.com/oauth/v2/auth');
+    }
+
+    /**
+     * Returns the access token API endpoint.
+     *
+     * @return UriInterface
+     */
+    public function getAccessTokenEndpoint()
+    {
+        return new Uri('https://netrunnerdb.com/oauth/v2/token');
+    }
+
+
+    protected function getAuthorizationMethod()
+    {
+        return static::AUTHORIZATION_METHOD_QUERY_STRING;
+    }
+
+    /**
+     * Adding a wrapper to the oauth request which tries the refresh token if the access token is expired.
+     * If that fails also, it logs out the user.
+     * @param $path
+     * @param string $method
+     * @param null $body
+     * @param array $extraHeaders
+     * @return null|string
+     */
+    public function requestWrapper($path, $method = 'GET', $body = null, array $extraHeaders = array())
+    {
+        try {
+            return $this->request($path, $method, $body, $extraHeaders);
+        } catch(\Exception $ex) {
+            // refresh token
+            try {
+                $token = $this->storage->retrieveAccessToken($this->service());
+                $this->refreshAccessToken($token);
+                return $this->request($path, $method, $body, $extraHeaders);
+            } catch(\Exception $ex) {
+                // if all fails logout user
+                Auth::logout();
+                return '{ "data": []}';
+            }
+        }
+    }
+}
