00322b556e38973169aa42da3d1cc871036ded81
madarasz <istvan.madarasz@disney.com>
2016-09-21 11:06:03 +0200

    updating error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.general')
+
+@section('content')
+    <h3 class="page-header">404 - Not found</h3>
+    <div class="col-xs-12 text-xs-center"><img src="/img/not-found.jpg"/></div>
+@stop
