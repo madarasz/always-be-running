25f0df1765903af5d80023ec790d2956a50da788
madarasz <istvan.madarasz@disney.com>
2016-10-21 12:11:07 +0200

    cookie consent

--- resources/views/layout/general.blade.php
+++ resources/views/layout/general.blade.php
@@ -22,6 +22,12 @@
     <link rel="stylesheet" href="/css/all.css">
     <link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,600' rel='stylesheet' type='text/css'>
     <script type="text/javascript" src="/js/all.js"></script>
+    <!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
+    <script type="text/javascript">
+        window.cookieconsent_options = {"message":"This website uses cookies to ensure you get the best experience on our website","dismiss":"Got it!","learnMore":"More info","link":null,"theme":"dark-bottom"};
+    </script>
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.10/cookieconsent.min.js"></script>
+    <!-- End Cookie Consent plugin -->
 </head>
 <body>
 
