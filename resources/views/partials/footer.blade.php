43d48ab4eaff43a2a660007ade8d99afdb02f12d
madarasz <istvan.madarasz@disney.com>
2016-10-25 13:34:24 +0200

    screen size hint in footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,4 +5,14 @@
             is copyrighted by Fantasy Flight Games and/or Wizards of the Coast.<br/>
             This website is not produced, endorsed, supported, or affiliated with Fantasy Flight Games and/or
             Wizards of the Coast.</p>
+    <p>
+        viewing on screensize:
+        <strong>
+            <span class="hidden-sm-up">xs</span>
+            <span class="hidden-xs-down hidden-md-up">sm</span>
+            <span class="hidden-sm-down hidden-lg-up">md</span>
+            <span class="hidden-md-down hidden-xl-up">lg</span>
+            <span class="hidden-lg-down">xl</span>
+        </strong>
+    </p>
 </footer>
