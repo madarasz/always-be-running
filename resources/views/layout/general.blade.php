b0a402dcefe4ac59ccbb973ffd457f13990a0c27
madarasz <istvan.madarasz@disney.com>
2016-11-04 12:38:33 +0100

    facebook meta tags

--- resources/views/layout/general.blade.php
+++ resources/views/layout/general.blade.php
@@ -28,6 +28,7 @@
     </script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.10/cookieconsent.min.js"></script>
     <!-- End Cookie Consent plugin -->
+    @include('partials.facebook-og')
 </head>
 <body>
 
---
+++ resources/views/partials/facebook-og.blade.php
@@ -0,0 +1,8 @@
+{{--meta tags for facebook open graph protocol--}}
+<meta property="og:title" content="Always be Running.net"/>
+<meta property="og:image" content="http://www.alwaysberunning.net/ms-icon-310x310.png" />
+@if (@$tournament)
+    <meta property="og:description" content="{!! substr(strip_tags(Markdown::convertToHtml($tournament->description)),0,200) !!}" />
+@else
+    <meta property="og:description" content="Tournament finder and tournament results for Android: Netrunner card game" />
+@endif
