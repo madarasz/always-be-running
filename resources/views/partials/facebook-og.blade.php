7b5d071d1c5a003dc3fb18ab16aaa64423fde32d
madarasz <istvan.madarasz@disney.com>
2016-11-04 12:48:46 +0100

    facebook preview fix v2

--- resources/views/partials/facebook-og.blade.php
+++ resources/views/partials/facebook-og.blade.php
@@ -2,7 +2,7 @@
 <meta property="og:title" content="Always be Running.net"/>
 <meta property="og:image" content="http://www.alwaysberunning.net/ms-icon-310x310.png" />
 @if (@$tournament)
-    <meta property="og:description" content="{!! substr($tournament->title.' - '.strip_tags(Markdown::convertToHtml($tournament->description)),0,200) !!}" />
+    <meta property="og:description" content="{!! substr($tournament->title.' - '.strip_tags(Markdown::convertToHtml($tournament->description)),0,300).'...' !!}" />
 @else
     <meta property="og:description" content="Tournament finder and tournament results for Android: Netrunner card game" />
 @endif
