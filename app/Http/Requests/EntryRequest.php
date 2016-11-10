0470b7223628b2838258eafe6447451983578ed8
madarasz <istvan.madarasz@disney.com>
2016-11-10 14:24:45 +0100

    claim with ID fix

--- app/Http/Requests/EntryRequest.php
+++ app/Http/Requests/EntryRequest.php
@@ -24,18 +24,22 @@ class EntryRequest extends Request
     public function rules()
     {
         return [
-            'corp_deck' => 'required',
-            'runner_deck' => 'required',
+            'corp_deck' => 'required_without:other_corp_deck',
+            'runner_deck' => 'required_without:other_runner_deck',
+            'other_corp_deck' => 'integer|min:1',
+            'other_runner_deck' => 'integer|min:1',
             'rank_top' => 'tournament_top:'.Request::get('rank').','.Request::get('top_number').'|tournament_not_top:'.Request::get('rank').','.Request::get('top_number')
         ];
     }
 
     public function messages() {
         return [
-            'corp_deck.required' => 'Create a corporation deck on NetrunnerDB, so you will be able to choose it here.',
-            'runner_deck.required' => 'Create a runner deck on NetrunnerDB, so you will be able to choose it here.',
+            'corp_deck.required_without' => 'Create a corporation deck on NetrunnerDB, so you will be able to choose it here.',
+            'runner_deck.required_without' => 'Create a runner deck on NetrunnerDB, so you will be able to choose it here.',
             'tournament_top' => "The \"rank after top cut\" must be \"below top cut\", because you didn't make the it to the top cut.",
-            'tournament_not_top' => "The \"rank after top cut\" must be set, because you made the top cut during the swiss rounds."
+            'tournament_not_top' => "The \"rank after top cut\" must be set, because you made the top cut during the swiss rounds.",
+            'other_corp_deck.integer' => "The corp deck ID must be a number. You can find it in its URL.",
+            'other_runner_deck.integer' => "The runner deck ID is a number. You can find it in its URL."
         ];
     }
 
