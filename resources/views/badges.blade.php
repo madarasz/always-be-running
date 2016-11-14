dab6bc375bf9af06c2d03aa27b562e91507b037b
madarasz <istvan.madarasz@disney.com>
2016-11-14 10:46:58 +0100

    badges page small fix

--- resources/views/badges.blade.php
+++ resources/views/badges.blade.php
@@ -16,7 +16,7 @@
                         <div class="col-xs-10">
                             <strong>{{ $badge->name }}</strong><br/>
                             {{ $badge->description }}<br/>
-                            <div class="small-text">(belonging to {{ $badge->users()->count() }} users)</div>
+                            <div class="small-text">(belonging to {{ $badge->users()->count() }} user{{ $badge->users()->count() > 1 ? 's' : '' }})</div>
                         </div>
                     </div>
                 @endforeach
