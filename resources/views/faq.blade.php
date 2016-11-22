6c522ab29041ee81de162134f84daf18cceb1d3d
madarasz <istvan.madarasz@disney.com>
2016-11-22 20:46:13 +0100

    nrtm update, faq / conclude dialog modifications

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -48,34 +48,54 @@
                     <a name="nrtm"></a><strong>How do I import the tournament results?</strong>
                 </p>
                 <p>
-                    You can either do it by NRTM or a CSV file that you prepare.
-                </p>
-                <p>
-                    <a href="https://itunes.apple.com/us/app/nrtm/id695468874?mt=8">NRTM</a> is a Netrunner tournament
-                    manager app. You will need the following to export tournament results:
+                    You can either do it by <strong>NRTM</strong> or a <strong>CSV</strong> file that you prepare.
                 </p>
                 <ul>
-                    <li>Get the identity names. Go to <strong>Settings</strong> >> <strong>Edit Names</strong> >>
-                        <strong>Download from NetrunnerDB.com</strong>.</li>
-                    <li>Add JSON export function. Go to <strong>Settings</strong> >> <strong>Data Export</strong> >>
-                        switch on <strong>Add JSON Data to Export</strong>.</li>
-                    <li>After the tournament is finished, go to <strong>More...</strong> >> <strong>Export</strong> >>>
-                        <img src="/img/icon_ios_upload.png"/> <strong>Export as Email / Dropbox</strong>.</li>
-                    <li>Upload the <strong>NRTM.json</strong> file you received when you conclude the tournament.</li>
+                    <li>
+                        <p>
+                            <a href="https://itunes.apple.com/us/app/nrtm/id695468874?mt=8">NRTM</a> is a Netrunner tournament
+                            manager app for iOS.</p>
+                        <p>
+                            First, download the identity names
+                            from NetrunnerDB by going to <strong>Settings</strong> >> <strong>Edit Names</strong> >>
+                            <strong>Download from NetrunnerDB.com</strong>. Then you can proceed with exporting the results:
+                        </p>
+                        <ul>
+                            <li>
+                                <img src="img/faq-nrtm1.png"/><br/>
+                                After the tournament is finished, go to tab <strong>More...</strong> >> <strong>Export</strong>.
+                                Click upload icon on top.<br/><br/>
+                                <img src="img/faq-nrtm2.png"/><br/>
+                                Select <strong>Upload to alwaysberunning.net</strong>.<br/><br/>
+                                <img src="img/faq-nrtm3.png"/><br/>
+                                After NRTM uploads the results, you will receive a <strong>conclusion code.</strong><br/><br/>
+                                <img src="img/faq-nrtm4.png"/><br/>
+                                When you are concluding the tournament on AlwaysBeRunning.net, provide this code.
+
+                            </li>
+                        </ul>
+                        <p class="p-t-1">
+                            Alternatively you can upload the NRTM.json results file. <br/>
+                            <em>(JSON export can be enabled by <strong>Settings</strong> >> <strong>Data Export</strong> >>
+                            switch on <strong>Add JSON Data to Export</strong>.)</em>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            If you are uploading a CSV file, you have to follow this format:<br/>
+                        <blockquote class="help-markdown">name;swiss-rank;topcut-rank;runnerID;corpID</blockquote><br/>
+                        If there were no top-cut or the player did not reach top-cut, use "0" (zero)
+                        in the <em>top-cut rank</em> field. The ID fields should be the (substring of the)
+                        official card name. Example:
+                        <blockquote class="help-markdown">
+                            Pete;1;0;Kit;Near-Earth Hub<br/>
+                            Tristan;2;0;Hayley;Engineering the Future<br/>
+                            Alice;3;0;Omar;Controlling the Message<br/>
+                            Ed;4;0;Khan;Architects of Tomorrow<br/>
+                        </blockquote>
+                        </p>
+                    </li>
                 </ul>
-                <p>
-                    If you are uploading a CSV file, you have follow this format:<br/>
-                    <blockquote class="help-markdown">name;swiss-rank;topcut-rank;runnerID;corpID</blockquote><br/>
-                    If there were no top-cut or the player did not reach top-cut, use "0" (zero)
-                    in the <em>top-cut rank</em> field. The ID fields should be the (substring of the)
-                    official card name. Example:
-                    <blockquote class="help-markdown">
-                        Pete;1;0;Kit;Near-Earth Hub<br/>
-                        Tristan;2;0;Hayley;Engineering the Future<br/>
-                        Alice;3;0;Omar;Controlling the Message<br/>
-                        Ed;4;0;Khan;Architects of Tomorrow<br/>
-                    </blockquote>
-                </p>
                 <p class="p-t-2">
                     <a name="more-questions"></a><strong>I have more questions or ideas.</strong>
                 </p>
