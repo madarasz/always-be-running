13eb3ac05e69aa75e178afd06ca2f1a833c6a68c
madarasz <istvan.madarasz@disney.com>
2016-12-08 13:53:14 +0100

    removing FB meta image

--- resources/views/partials/facebook-og.blade.php
+++ resources/views/partials/facebook-og.blade.php
@@ -4,5 +4,5 @@
     <meta property="og:description" content="{!! substr($tournament->title.' - '.strip_tags(Markdown::convertToHtml($tournament->description)),0,300).'...' !!}" />
 @else
     <meta property="og:description" content="Tournament finder and tournament results for Android: Netrunner card game" />
-    <meta property="og:image" content="https://www.alwaysberunning.net/ms-icon-310x310.png" />
+    {{--<meta property="og:image" content="https://www.alwaysberunning.net/ms-icon-310x310.png" />--}}
 @endif
