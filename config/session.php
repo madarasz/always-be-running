68a159f13641325d04a864dc00e33146da96ebd0
madarasz <istvan.madarasz@disney.com>
2017-03-03 10:05:03 +0100

    session to DB, session tweeks

--- config/session.php
+++ config/session.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'lifetime' => 120,
+    'lifetime' => 20160,
 
     'expire_on_close' => false,
 
@@ -70,7 +70,7 @@ return [
     |
     */
 
-    'connection' => null,
+    'connection' => 'mysql',
 
     /*
     |--------------------------------------------------------------------------
