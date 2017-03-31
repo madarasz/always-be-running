a79fd0666cd8201b9a06baef4d85ecc4fe9bcc4e
madarasz <Sajt123*>
2017-03-31 09:12:03 +0200

    icons legal update

--- resources/views/partials/legal-icons.blade.php
+++ resources/views/partials/legal-icons.blade.php
@@ -7,6 +7,10 @@
         <a href="http://simpleicon.com/" rel="nofollow">SimpleIcon</a>,
         <a href="http://pixelbazaar.com/preview" rel="nofollow">Zlatko Najdenovski</a>,
         <a href="https://www.epicpxls.com/" rel="nofollow">EpicCoders</a>
-        &nbsp;from&nbsp; <a href="http://www.flaticon.com/" rel="nofollow">http://www.flaticon.com/</a>.
+        &nbsp;from&nbsp; <a href="http://www.flaticon.com/" rel="nofollow">http://www.flaticon.com/</a>.<br/>
+        Card illustrations by <a href="http://www.mattzeilinger.com/">Matt Zeilinger</a>,
+        <a href="http://kaenlogos.deviantart.com/">Ralph Beisner</a>,
+        <a href="http://www.gongstudios.com/">Gong Studios</a> and
+        <a href="http://viccolatte.deviantart.com/">Viktoria Gavrilenko</a>.
     </div>
 </div>
