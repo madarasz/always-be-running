03a800271b79370e58c4f9461244cbe8fa0337c4
madarasz <Sajt123*>
2017-06-06 12:15:09 +0200

    facebook image fix

--- resources/views/partials/facebook-og.blade.php
+++ resources/views/partials/facebook-og.blade.php
@@ -2,11 +2,12 @@
 <meta property="og:title" content="Always be Running.net"/>
 @if (@$tournament)
     <meta property="og:description" content="{!! substr($tournament->title.' - '.strip_tags(Markdown::convertToHtml($tournament->description)),0,300).'...' !!}" />
-    <?php $containsImage = preg_match('/!\[([^\]]*)\]\(([^)]+)\)/', $tournament->description); ?>
-    @if (!$containsImage)
-        <meta property="og:image" content="https://alwaysberunning.net/ms-icon-70x70.png" />
+    @if ($tournament->coverImage())
+        <meta property="og:image" content="{{ $tournament->coverImage() }}" />
+    @else
+        <meta property="og:image" content="https://alwaysberunning.net/ms-icon-310x310.png" />
     @endif
 @else
     <meta property="og:description" content="Tournament finder and tournament results for Android: Netrunner card game" />
-    <meta property="og:image" content="https://alwaysberunning.net/ms-icon-70x70.png" />
+    <meta property="og:image" content="https://alwaysberunning.net/ms-icon-310x310.png" />
 @endif
