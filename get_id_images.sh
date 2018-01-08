9dfcff3df3ae6b614f521bb4fd1dc28fd388e299
madarasz <Sajt123*>
2018-01-08 11:35:00 +0100

    ID image grabber update: more resolution, support cardgamedb URLs

--- get_id_images.sh
+++ get_id_images.sh
@@ -5,6 +5,7 @@ curl https://netrunnerdb.com/api/2.0/public/cards > cards.json
 types=( $(cat cards.json | jq -r '.data[].type_code' ) )
 sides=( $(cat cards.json | jq -r '.data[].side_code' ) )
 codes=( $(cat cards.json | jq -r '.data[].code' ) )
+imageurls=( $(cat cards.json | jq -r '.data[].image_url' ) )
 titles=( $(cat cards.json | jq -r '.data[].title' | tr -s ' ' | tr ' ' '-' | tr '[:upper:]' '[:lower:]' | sed "s/[^a-z0-9.-]//g") )
 i=0;
 for card in "${codes[@]}"
@@ -13,14 +14,19 @@ do
         title=${titles[$i]}
         if [ ! -f "public/img/ids/$card.png" ]; then
             image=${cards[$i]}
-            echo "Downloading card image: $title"
-            curl "https://netrunnerdb.com/card_image/$card.png" > "public/img/ids/$card.png"
+            imageurl=${imageurls[$i]}
+            echo "Downloading card image: $title - $imageurl"
+            if [ "$imageurl" == "null" ]; then
+                curl "https://netrunnerdb.com/card_image/$card.png" > "public/img/ids/$card.png"
+            else
+                curl $imageurl > "public/img/ids/$card.png"
+            fi
             if [ ${sides[$i]} == "corp" ]; then
                 echo corp
-                mogrify -crop 224x224+38+67 -resize 20x20 "public/img/ids/$card.png"
+                mogrify -crop 224x224+38+67 -resize 40x40 "public/img/ids/$card.png"
             else
                 echo runner
-                mogrify -crop 238x238+31+51 -resize 20x20 "public/img/ids/$card.png"
+                mogrify -crop 238x238+31+51 -resize 40x40 "public/img/ids/$card.png"
             fi
         fi
     fi
