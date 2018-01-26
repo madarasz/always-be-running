c219b7649e11f9964462b8e686461dc3dac0131e
madarasz <Sajt123*>
2018-01-26 16:49:55 +0100

    removing tournament type text from created tournaments list on Profiles

--- resources/views/profile/created.blade.php
+++ resources/views/profile/created.blade.php
@@ -12,7 +12,6 @@
                     {{ $tournament->title }}
                 </a><br/>
                 <div class="small-text">
-                    {{ $tournament->tournament_type()->first()->type_name }} -
                     @if($tournament->tournament_type_id != 7)
                         {{ $tournament->location_country }}, {{$tournament->location_country === 'United States' ? $tournament->location_state.', ' : ''}}{{ $tournament->location_city }}
                     @else
