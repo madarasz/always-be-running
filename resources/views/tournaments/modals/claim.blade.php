44a45a655c4c8d65f42afb4dad3c5c44843a418d
madarasz <mr.madarasz.istvan@gmail.com>
2018-02-28 11:32:49 +0100

    disabling adding claim to NetrunnerDB

--- resources/views/tournaments/modals/claim.blade.php
+++ resources/views/tournaments/modals/claim.blade.php
@@ -151,7 +151,7 @@
                                 <hr/>
                                 <div class="row">
                                     <div class="col-xs-12 text-xs-center">
-                                        {!! Form::checkbox('netrunnerdb_link', null, env('DEFAULT_NETRUNNERDB_CLAIM'), ['id' => 'netrunnerdb_link']) !!}
+                                        {!! Form::checkbox('netrunnerdb_link', null, env('DEFAULT_NETRUNNERDB_CLAIM'), ['id' => 'netrunnerdb_link', 'disabled' => '']) !!}
                                         {!! Form::label('netrunnerdb_link', 'add claim to decklists on NetrunnerDB', ['class' => 'm-b-0']) !!}
                                         @include('partials.popover', ['direction' => 'bottom', 'content' =>
                                             'Selecting this option will also add your claim to the decklist page of NetrunnerDB.
