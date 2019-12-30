932cb336d2509bde7f6d170cf7220fdd7d6d2dd6
madarasz <mr.madarasz.istvan@gmail.com>
2019-12-30 16:17:43 +0100

    other art responsive adjustment

--- resources/views/prizes/other.blade.php
+++ resources/views/prizes/other.blade.php
@@ -23,7 +23,7 @@
                 <div class="markdown-content" v-html="markdownDescription( artist.description)">
                 </div>
                 <div class="row">
-                    <div class="col-xs-12 col-lg-6 p-t-1 p-b-1 checkered-lg" v-for="(item, index) in artist.items">
+                    <div class="col-xs-12 col-md-6 p-t-1 p-b-1 checkered-md" v-for="(item, index) in artist.items">
                         <div class="flex-row">
                             <div class="gallery-item" style="margin: 0" v-for="photo in item.photos">
                                 <div style="position: relative;">
