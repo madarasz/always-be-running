69ade57df3371f88449d307164d275adac2808a1
madarasz <mr.madarasz.istvan@gmail.com>
2021-04-27 20:44:22 +0200

    ID thumbnail downloader script update

--- get_id_images.sh
+++ get_id_images.sh
@@ -8,7 +8,6 @@ curl https://netrunnerdb.com/api/2.0/public/cards > cards.json
 types=( $(cat cards.json | jq -r '.data[].type_code' ) )
 sides=( $(cat cards.json | jq -r '.data[].side_code' ) )
 codes=( $(cat cards.json | jq -r '.data[].code' ) )
-imageurls=( $(cat cards.json | jq -r '.data[].image_url' ) )
 titles=( $(cat cards.json | jq -r '.data[].title' | tr -s ' ' | tr ' ' '-' | tr '[:upper:]' '[:lower:]' | sed "s/[^a-z0-9.-]//g") )
 i=0;
 
@@ -19,49 +18,26 @@ do
         title=${titles[$i]}
         # download if not already present
         if [ ! -f "public/img/ids/$card.png" ]; then
-            imageurl=${imageurls[$i]}
-            echo "Downloading card image: $title - $imageurl"
-
-            # download
-            if [ "$imageurl" == "null" ]; then
-                # download from NetrunnerDB
-                if [ $card -lt 31001 ]; then
-                  curl "https://netrunnerdb.com/card_image/$card.png" > "public/img/ids/$card.png"
-                else
-                  curl "https://netrunnerdb.com/card_image/large/$card.jpg" > "public/img/ids/$card.png"
-                  mogrify -resize 372x520 "public/img/ids/$card.png"
-                fi
-            else
-                # download from CardGameDB
-                curl $imageurl > "public/img/ids/$card.png"
-            fi
+            echo "Downloading card image: $title"
+            curl "https://netrunnerdb.com/card_image/large/$card.jpg" > "public/img/ids/$card.png"
             # crop and resize
             if [ $card -lt 26000 ]; then
-                # old images
+                # old FFG ID templates
                 if [ ${sides[$i]} == "corp" ]; then
                     echo corp
-                    mogrify -crop 224x224+38+67 -resize 80x80 "public/img/ids/$card.png"
+                    mogrify -crop 215x215+43+68 -resize 80x80 "public/img/ids/$card.png"
                 else
                     echo runner
-                    mogrify -crop 238x238+31+51 -resize 80x80 "public/img/ids/$card.png"
-                fi
-            elif [ $card -lt 30001 ]; then
-                # old NISEI images
-                if [ ${sides[$i]} == "corp" ]; then
-                    echo corp
-                    mogrify -crop 355x355+48+75 -resize 80x80 "public/img/ids/$card.png"
-                else
-                    echo runner
-                    mogrify -crop 330x330+62+88 -resize 80x80 "public/img/ids/$card.png"
+                    mogrify -crop 232x232+33+56 -resize 80x80 "public/img/ids/$card.png"
                 fi
             else
-                # new NISEI images
+                # new NISEI ID templates
                 if [ ${sides[$i]} == "corp" ]; then
                     echo corp
-                    mogrify -crop 266x266+53+57 -resize 80x80 "public/img/ids/$card.png"
+                    mogrify -crop 237x237+30+46 -resize 80x80 "public/img/ids/$card.png"
                 else
                     echo runner
-                    mogrify -crop 266x266+53+61 -resize 80x80 "public/img/ids/$card.png"
+                    mogrify -crop 226x226+39+56 -resize 80x80 "public/img/ids/$card.png"
                 fi
             fi
         fi
