49523fd7d1b90964ac2c5b9196b613787eb69077
madarasz <mr.madarasz.istvan@gmail.com>
2025-01-03 14:38:54 +0100

    small fixes

--- resources/views/tournaments/partials/pager.blade.php
+++ resources/views/tournaments/partials/pager.blade.php
@@ -11,7 +11,7 @@
 </div>
 <div class="text-xs-center hidden-xs-up" id="{{ $id }}-options" style="font-size: 80%; font-style: normal"
         data-selected="{{ @$pager_options[0] }}">
-    @if (count(@$pager_options)>0)
+    @if (isset($pager_options) && is_array($pager_options) && count($pager_options) > 0)
         @foreach($pager_options as $key=>$option)
             <span class="label control-paging {{ $key==0 ? 'label-active':'label-inactive' }}"
                   onclick="changePageOptions('{{$id}}', '{{ $option }}')">
@@ -20,7 +20,7 @@
         @endforeach
     @endif
     {{--country flag / text switcher--}}
-    @if (@$flag_switcher)
+    @if (isset($flag_switcher) && $flag_switcher)
         <span> - </span>
         <span class="label control-flag label-active" onclick="changeFlagOption(true)">flag</span>
         <span class="label control-text label-inactive" onclick="changeFlagOption(false)">text</span>
