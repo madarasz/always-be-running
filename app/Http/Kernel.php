585711296e5c38915174601b2a051ad5445bf905
madarasz <mr.madarasz.istvan@gmail.com>
2026-03-08 00:40:16 +0100

    Migrate to Laravel 5.6

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
         \App\Http\Middleware\XSSProtection::class
     ];
